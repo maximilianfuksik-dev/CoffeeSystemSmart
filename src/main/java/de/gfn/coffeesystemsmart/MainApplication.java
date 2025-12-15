39d98f64a55568fb7568eb2639b3092fbb3152f4
MD-43700659576\Student <pat_gros@hotmail.com>
2025-12-15 16:17:46 +0100

    Patrick bilder anpassung

--- src/main/java/de/gfn/coffeesystemsmart/MainApplication.java
+++ src/main/java/de/gfn/coffeesystemsmart/MainApplication.java
@@ -14,7 +14,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load()); // Fenster-Größe rausgenommen, damit es richtig angezeigt wird
-        stage.setTitle("Hello!");
+        stage.setTitle("Smart Coffee");
         stage.setScene(scene);
         stage.show();
         Logger log = new Logger("MainApplication");
