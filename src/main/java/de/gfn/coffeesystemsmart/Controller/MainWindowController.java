3b128f2161ced247b52e7e40446f1fa099b76feb
MD-43700659576\Student <pat_gros@hotmail.com>
2025-12-15 11:47:15 +0100

    Patrick Main-Window verbunden und properties erstellt.

--- src/main/java/de/gfn/coffeesystemsmart/Controller/MainWindowController.java
+++ src/main/java/de/gfn/coffeesystemsmart/Controller/MainWindowController.java
@@ -5,10 +5,6 @@ import javafx.scene.control.Label;
 
 public class MainWindowController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label timeDate;
 }
+
