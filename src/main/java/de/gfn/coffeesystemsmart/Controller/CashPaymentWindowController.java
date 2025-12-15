e336a2b0ed36dbd9a9fbeb0310e1eb2864ee819a
MD-43700659576\Student <pat_gros@hotmail.com>
2025-12-15 15:17:16 +0100

    Patrick CashPaymentWindow fertig

--- src/main/java/de/gfn/coffeesystemsmart/Controller/CashPaymentWindowController.java
+++ src/main/java/de/gfn/coffeesystemsmart/Controller/CashPaymentWindowController.java
@@ -1,4 +1,21 @@
 package de.gfn.coffeesystemsmart.Controller;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class CashPaymentWindowController {
+    @FXML
+    private Label pricing;
+
+    /*
+    Methoden für onActionPress:
+    btn.twoEur
+    btn.oneEur
+    btn.fiftyCent
+    btn.twentyCent
+    btn.tenCent
+    btn.fiveCent
+    */
+
 }
