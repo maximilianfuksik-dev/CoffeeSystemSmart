185da9e09b37c68384a267f778dccf17a35bbaa1
MD-43700659576\Student <pat_gros@hotmail.com>
2025-12-16 08:40:07 +0100

    Patrick Exit Button der main hinzugefügt

--- src/main/java/de/gfn/coffeesystemsmart/Controller/MainWindowController.java
+++ src/main/java/de/gfn/coffeesystemsmart/Controller/MainWindowController.java
@@ -3,8 +3,18 @@ package de.gfn.coffeesystemsmart.Controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import static javafx.application.Platform.exit;
+
+
 public class MainWindowController {
+
     @FXML
     private Label timeDate;
+
+    @FXML
+    public void btnExit() {
+        exit();
+    }
+
 }
 
