cd65cf98630a16969daf3668138b8e33a7698749
MD-43700659576\Student <pat_gros@hotmail.com>
2025-12-16 15:20:40 +0100

    Patrick Settings Buttons (Bericht erstellen und Änderungen + Maschine ausschalten) funktionierten jetzt.

--- src/main/java/de/gfn/coffeesystemsmart/Controller/SettingsWindowController.java
+++ src/main/java/de/gfn/coffeesystemsmart/Controller/SettingsWindowController.java
@@ -2,6 +2,7 @@ package de.gfn.coffeesystemsmart.Controller;
 
 import de.gfn.coffeesystemsmart.Classes.LanguageChange;
 import de.gfn.coffeesystemsmart.MainApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +11,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Locale;
+
+import static javafx.application.Platform.exit;
 
 public class SettingsWindowController {
 
@@ -24,6 +27,40 @@ public class SettingsWindowController {
         this.settingsStage = settingsStage;
     }
 
+    // Implementierung der Methoden für Reperatur und Auffüllen, von Kaffee und Milch.
+
+    @FXML
+    public void btnPrintReport(ActionEvent event) throws IOException {
+        FXMLLoader chartLoader = new FXMLLoader(MainApplication.class.getResource("chart-window.fxml"));
+        chartLoader.setResources(LanguageChange.getBundle());
+
+        Scene settings = new Scene(chartLoader.load());
+
+        Stage settingsStage = new Stage();
+        settingsStage.setTitle(LanguageChange.getBundle().getString("label.printReport"));
+        settingsStage.setScene(settings);
+        settingsStage.show();
+    }
+
+    @FXML
+    public void btnChanges(ActionEvent event) throws IOException {
+        FXMLLoader chartLoader = new FXMLLoader(MainApplication.class.getResource("setting-change-window.fxml"));
+        chartLoader.setResources(LanguageChange.getBundle());
+
+        Scene settings = new Scene(chartLoader.load());
+
+        Stage settingsStage = new Stage();
+        settingsStage.setTitle(LanguageChange.getBundle().getString("label.changes"));
+        settingsStage.setScene(settings);
+        settingsStage.show();
+    }
+
+    @FXML
+    public void btnTurnOffMachine(){ exit(); }
+
+
+
+    // Ab hier Methoden für Sprachpakete:
     @FXML
     private void switchToGerman() throws IOException {
         switchLanguage(Locale.GERMAN);
